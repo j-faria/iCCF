fix derivatives; erros on parameters; utility function for indicators from list of files

iCCF/gaussian.py
@@ -8,41 +8,72 @@ def gauss(x, p):
     return p[0] * exp(-(x - p[1])**2 / (2 * p[2]**2)) + p[3]
 
 
-def gauss_partial_deriv(x, p):
-    """ Partial derivatives of a Gaussian with respect to each parameter """
-    A, x0, sig, offset = p
-    g = gauss(x, p)
+def _gauss_partial_deriv(x, p):
+    """ Partial derivatives of a Gaussian with respect to each parameter. """
+    A, x0, sig, _ = p
+    g = gauss(x, [A, x0, sig, 0.0])
     dgdA = gauss(x, [1.0, x0, sig, 0.0])
-    dgdx0 = dgdA * ((x-x0)/sig**2)
-    dgdsig = dgdA * ((x-x0)**2/sig**3)
+    dgdx0 = g * ((x-x0)/sig**2)
+    dgdsig = g * ((x-x0)**2/sig**3)
     dgdoffset = np.ones_like(x)
     return np.c_[dgdA, dgdx0, dgdsig, dgdoffset]
 
 
-def gaussfit(x, y, p0=None):
+def _gauss_initial_guess(x, y):
+    """ Educated guess (from the data) for Gaussian parameters. """
+    p0 = []
+    p0.append(y.mean() - y.max())  # guess the amplitude
+    # guess the center, but maybe the CCF is upside down?
+    if y[x.size // 2] > y[0]:  # seems like it
+        p0.append(x[y.argmax()])
+    else:
+        p0.append(x[y.argmin()])
+    p0.append(1)  # guess the sigma
+    p0.append(y.mean())  # guess the offset 
+    return p0
+
+def gaussfit(x, y, p0=None, return_errors=False, use_deriv=True):
     """ 
     Fit a Gaussian function to `x`,`y` using least-squares, with initial guess
     `p0` = [A, x0, σ, offset]. If p0 is not provided, the function tries an
     educated guess, which might lead to bad results.
+
+    Parameters
+    ----------
+    x : array
+        The independent variable where the data is measured
+    y : array
+        The dependent data.
+    p0 : list or array
+        Initial guess for the parameters. If None, try to guess them from x,y.
+    return_errors : bool
+        Whether to return estimated errors on the parameters.
+    use_deriv : bool
+        Whether to use partial derivatives of the Gaussian (wrt the parameters)
+        as Jacobian in the fit. If False, the Jacobian will be estimated.
     """
-    f = lambda p, x, y: gauss(x, p) - y
     # f = lambda x, A, x0, sig, offset: gauss(x, [A, x0, sig, offset])
+    f = lambda p, x, y: gauss(x, p) - y
+    if use_deriv:
+        df = lambda p, x, y: _gauss_partial_deriv(x, p)
+    else:
+        df = None
     
     if p0 is None:
-        p0 = []
-        p0.append(y.mean() - y.max())  # guess the amplitude
-        # guess the center, but maybe the ccf is upside down?
-        if y[x.size // 2] > y[0]:  # seems like it
-            p0.append(x[y.argmax()])
-        else:
-            p0.append(x[y.argmin()])
-        p0.append(1)  # guess the sigma
-        p0.append(y.mean())  # guess the offset
+        p0 = _gauss_initial_guess(x, y)
     
-    result = optimize.leastsq(f, p0, args=(x, y))
+    result = optimize.leastsq(f, p0, args=(x, y), Dfun=df, full_output=True)
+    pfit, pcov, infodict, errmsg, success = result
 
-    result[0][2] = abs(result[0][2]) # positive sigma
-    return result[0]
+    pfit[2] = abs(pfit[2]) # positive sigma
+
+    if return_errors:
+        s_sq = (f(pfit, x, y)**2).sum() / (y.size - pfit.size)
+        pcov = pcov * s_sq
+        errors = np.sqrt(np.diag(pcov))
+        return pfit, errors
+    else:
+        return pfit
     # bounds = ([-inf, -inf, 0, -inf], [inf, inf, inf, inf])
     # return optimize.curve_fit(f, x, y, p0=p0, bounds=bounds)[0]
 

iCCF/utils.py
@@ -1,7 +1,6 @@
 import re
 import numpy as np
 
-
 # from https://stackoverflow.com/a/30141358/1352183
 def running_mean(x, N=2):
     cumsum = np.cumsum(np.insert(x, 0, 0))
@@ -10,4 +9,4 @@ def running_mean(x, N=2):
 
 # from https://stackoverflow.com/a/16090640
 def natsort(s):
-    return [int(t) if t.isdigit() else t.lower() for t in re.split('(\d+)', s)]
+    return [int(t) if t.isdigit() else t.lower() for t in re.split(r'(\d+)', s)]