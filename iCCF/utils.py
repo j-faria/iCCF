87f0f1043972c820b7584fd8ec65d78b7b7ea052
João Faria <joao.faria@astro.up.pt>
2019-01-15 17:09:08 +0000

    might be useful in the future

--- iCCF/utils.py
+++ iCCF/utils.py
@@ -10,3 +10,24 @@ def running_mean(x, N=2):
 # from https://stackoverflow.com/a/16090640
 def natsort(s):
     return [int(t) if t.isdigit() else t.lower() for t in re.split(r'(\d+)', s)]
+
+
+def numerical_gradient(rv, ccf):
+    """
+    Return the gradient of the CCF.
+
+    Parameters
+    ----------
+    rv : array
+        The velocity values where the CCF is defined.
+    ccf : array
+        The values of the CCF profile.
+
+    Notes
+    -----
+    The gradient is computed using the np.gradient routine, which uses second
+    order accurate central differences in the interior points and either first
+    or second order accurate one-sides (forward or backwards) differences at
+    the boundaries. The gradient has the same shape as the input array.
+    """
+    return np.gradient(ccf, rv)
