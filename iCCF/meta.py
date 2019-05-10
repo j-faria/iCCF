bug fix in CCF calculation

iCCF/meta.py
@@ -101,29 +101,41 @@ def makeCCF(spec_wave, spec_flux, mask_wave=None, mask_contrast=None, mask=None,
 
     Parameters
     ----------
-    spec_wave : array The wavelength of the observed spectrum. spec_flux : array
-        The flux of the observed spectrum. mask_wave : array, optional The
-        wavelength of the mask. mask_contrast : array, optional The flux
-        (contrast) of the mask. mask : array (..., 3), optional The mask as an
-        array with lambda1, lambda2, depth rvmin : float, optional Minimum
-        radial velocity for which to calculate the CCF [km/s]. rvmax : float,
-        optional Maximum radial velocity for which to calculate the CCF [km/s].
-        drv : float, optional The radial-velocity step [km/s]. rvarray : array,
-        optional The radial velocities at which to calculate the CCF. If this is
+    spec_wave : array
+        The wavelength of the observed spectrum.
+    spec_flux : array
+        The flux of the observed spectrum.
+    mask_wave : array, optional
+        The central wavelength of the mask.
+    mask_contrast : array, optional
+        The flux (contrast) of the mask.
+    mask : array (..., 3), optional
+        The mask as an array with lambda1, lambda2, depth.
+    mask_width : float, optional [default=0.82]
+        Width of the mask "holes", in velocity in km/s.
+    rvmin : float, optional
+        Minimum radial velocity for which to calculate the CCF [km/s].
+    rvmax : float, optional
+        Maximum radial velocity for which to calculate the CCF [km/s].
+    drv : float, optional
+        The radial-velocity step [km/s].
+    rvarray : array, optional
+        The radial velocities at which to calculate the CCF [km/s]. If this is
         provided, `rvmin`, `rvmax` and `drv` are ignored.
 
     Returns
     -------
     rv : array The radial-velocity where the CCF was calculated [km/s]. These
-        RVs refer to a shift of the template -- positive values indicate that
-        the template has been red-shifted and negative numbers indicate a
-        blue-shift of the template. ccf : array The values of the
-        cross-correlation function.
+        RVs refer to a shift of the mask -- positive values indicate that the
+        mask has been red-shifted and negative numbers indicate a blue-shift of
+        the mask.
+    ccf : array
+        The values of the cross-correlation function.
     """
     if rvarray is None:
         if rvmin is None or rvmax is None or drv is None:
             raise ValueError("Provide `rvmin`, `rvmax`, and `drv`.")
-        # Check order of rvmin and rvmax
+        # check order of rvmin and rvmax
         if rvmax <= rvmin:
             raise ValueError("`rvmin` should be smaller than `rvmax`.")
         rvarray = np.arange(rvmin, rvmax + drv / 2, drv)
@@ -131,6 +143,11 @@ def makeCCF(spec_wave, spec_flux, mask_wave=None, mask_contrast=None, mask=None,
     wave_resolution = spec_wave[1] - spec_wave[0]
 
     if mask is None:
+        if mask_wave is None:
+            raise ValueError("Provide the mask wavelengths in `mask_wave`.")
+        if mask_contrast is None:
+            raise ValueError("Provide the mask wavelengths in `mask_contrast`.")
+
         mask = np.c_[doppler_shift_wave(mask_wave, -mask_width / 2),
                     doppler_shift_wave(mask_wave, mask_width / 2), mask_contrast]
 
@@ -142,28 +159,32 @@ def makeCCF(spec_wave, spec_flux, mask_wave=None, mask_contrast=None, mask=None,
         mask_rv_shifted = np.copy(mask)
         mask_rv_shifted[:, :2] = doppler_shift_wave(mask[:, :2], RV)
 
-        region = (spec_wave[0] < mask_rv_shifted[:, 0]) & (
-            mask_rv_shifted[:, 1] < spec_wave[-1])
+        # region of intersection between the RV-shifted mask and the spectrum
+        region = (spec_wave[0] < mask_rv_shifted[:, 0]) & (mask_rv_shifted[:, 1] < spec_wave[-1])
         mask_rv_shifted = mask_rv_shifted[region]
 
+        # for every line in the mask
         for mask_line_start, mask_line_end, mask_line_depth in mask_rv_shifted:
+
             if mask_line_end + wave_resolution >= spec_wave[-1]:
                 break
 
+            # find the limiting indices in spec_wave, corresponding to the start
+            # and end wavelength of the mask
             linePixelIni = bisect_left(spec_wave, mask_line_start)
             linePixelEnd = bisect_right(spec_wave, mask_line_end)
 
-            lineFractionIni = (
-                spec_wave[linePixelIni] - mask_line_start) / wave_resolution
-            lineFractionEnd = (
-                mask_line_end - spec_wave[linePixelEnd]) / wave_resolution
+            # fraction of the spectrum inside the mask hole at the start
+            lineFractionIni = (spec_wave[linePixelIni] - mask_line_start) / wave_resolution
+            # fraction of the spectrum inside the mask hole at the end
+            lineFractionEnd = 1 - abs(mask_line_end - spec_wave[linePixelEnd]) / wave_resolution
 
-            CCF += mask_line_depth * (
-                np.sum(spec_flux[linePixelIni:linePixelEnd]) +
-                lineFractionIni * spec_flux[linePixelIni - 1] +
-                lineFractionEnd * spec_flux[linePixelEnd + 1])
+            CCF += mask_line_depth * np.sum(spec_flux[linePixelIni:linePixelEnd])
+            CCF += mask_line_depth * lineFractionIni * spec_flux[linePixelIni - 1]
+            CCF += mask_line_depth * lineFractionEnd * spec_flux[linePixelEnd + 1]
             nlines += 1
 
         ccfarray[i] = CCF
 
-    return ccfarray
+    return rvarray, ccfarray
+