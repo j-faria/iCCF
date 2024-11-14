8893d580267367d9758341976be113a725858f42
João Faria <joao.faria@unige.ch>
2024-11-14 12:10:18 +0100

    revamping the functions to get keywords

--- iCCF/keywords.py
+++ iCCF/keywords.py
@@ -5,6 +5,11 @@ __all__ = ['getRV', 'getRVerror', 'getRVarray',
            'getBJD', 'getFWHM', 'getMASK', 'getINSTRUMENT']
 
 
+def _check_hdul(hdul, fitsfile, **kwargs):
+    if hdul is None:
+        hdul = _get_hdul(fitsfile, **kwargs)
+    return hdul
+
 def _try_keywords(hdul, *keywords, exception=None):
     for kw in keywords:
         try:
@@ -14,88 +19,64 @@ def _try_keywords(hdul, *keywords, exception=None):
     return None
 
 
-def getRV(fitsfile, hdul=None, keyword=None, return_hdul=False, **kwargs):
-    """
-    Try to find the radial velocity in the header of `fitsfile`. If `keyword`
-    is not provided, search for the following keywords
+class getKW:
+    def __init__(self, name, kws):
+        self.name = name
+        self.kws = kws
+    def __call__(self, fitsfile, hdul=None, keyword=None, return_hdul=False, **kwargs):
+        _check_hdul(hdul, fitsfile, **kwargs)
+        if keyword is not None:
+            return hdul[0].header[keyword]
+        val = _try_keywords(hdul, *self.kws)
+        if val is not None:
+            if return_hdul:
+                return val, hdul
+            else:
+                return val
+        obj = fitsfile or hdul
+        fail = ValueError(f'Could not find any {self.name} keyword in header of "{obj}"')
+        raise fail
+
+
+getRV = getKW('RV', ['HIERARCH ESO QC CCF RV', 'HIERARCH ESO DRS CCF RVC'])
+getRV.__doc__ = f"""
+Try to find the radial velocity in the header of `fitsfile`. If `keyword`
+is not provided, search for the following keywords
     - HIERARCH ESO QC CCF RV
     - HIERARCH ESO DRS CCF RVC
 
-    Arguments
-    ---------
-    fitsfile : str:
+Args:
+    fitsfile (str):
         The name of the fits file
-    hdul : astropy.fits.HDUList (optional)
+    hdul (astropy.fits.HDUList, optional):
         If provided, ignore `fitsfile` and use this HDU list directly
-    keyword : str (optional)
+    keyword (str, optional):
         Get this keyword from the header instead of looking for RV keywords
-    return_hdul: bool (optional, default False)
+    return_hdul: (bool, optional)
         Whether to return the HDU list read from the file
-    """
-    if hdul is None:
-        hdul = _get_hdul(fitsfile, **kwargs)
-
-    if keyword is not None:
-        return hdul[0].header[keyword]
-
-    # need to look for it
-    kws = [
-        'HIERARCH ESO QC CCF RV', 
-        'HIERARCH ESO DRS CCF RVC'
-    ]
-    val = _try_keywords(hdul, *kws)
-
-    if val is not None:
-        if return_hdul:
-            return val, hdul
-        else:
-            return val
-
-    obj = fitsfile or hdul
-    fail = ValueError(f'Could not find any RV keyword in header of "{obj}"')
-    raise fail
+"""
 
 
-def getRVerror(fitsfile, hdul=None, keyword=None, return_hdul=False, **kwargs):
-    """
-    Try to find the radial velocity uncertainty in the header of `fitsfile`. If 
-    `keyword` is not provided, search for the following keywords
+getRVerror = getKW('RV error', ['HIERARCH ESO QC CCF RV ERROR', 'HIERARCH ESO DRS CCF NOISE'])
+getRVerror.__doc__ = f"""
+Try to find the radial velocity uncertainty in the header of `fitsfile`. If
+`keyword` is not provided, search for the following keywords
     - HIERARCH ESO QC CCF RV ERROR
     - HIERARCH ESO DRS CCF NOISE
 
-    Arguments
-    ---------
-    fitsfile : str:
+Args:
+    fitsfile (str):
         The name of the fits file
-    hdul : astropy.fits.HDUList (optional)
+    hdul (astropy.fits.HDUList, optional):
         If provided, ignore `fitsfile` and use this HDU list directly
-    keyword : str (optional)
+    keyword (str, optional):
         Get this keyword from the header instead of looking for RV keywords
-    return_hdul: bool (optional, default False)
+    return_hdul: (bool, optional)
         Whether to return the HDU list read from the file
-    """
-    if hdul is None:
-        hdul = _get_hdul(fitsfile, **kwargs)
+"""
 
-    if keyword is not None:
-        return hdul[0].header[keyword]
 
-    # need to look for it
-    kws = [
-        'HIERARCH ESO QC CCF RV ERROR', 
-        'HIERARCH ESO DRS CCF NOISE'
-    ]
-    val = _try_keywords(hdul, *kws)
-
-    if val is not None:
-        if return_hdul:
-            return val, hdul
-        else:
-            return val
-
-    obj = fitsfile or hdul
-    fail = ValueError(f'Could not find any RV ERROR keyword in header of "{obj}"')
-    raise fail
+getBIS = getKW('BIS', ['HIERARCH ESO QC CCF BIS SPAN'])
 
 
 def getRVarray(fitsfile, hdul=None, return_hdul=False, **kwargs):
